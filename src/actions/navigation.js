a82a5be53cdcd1d84feb3ea49867d02b847f0149
Chiew Carol <kyaroru@outlook.com>
2017-12-28 14:22:06 +0800

    Fixed undefined issue when logout

--- src/actions/navigation.js
+++ src/actions/navigation.js
@@ -3,12 +3,15 @@ const NAVIGATION = 'NAVIGATION';
 export const getNavigation = store => store[NAVIGATION];
 
 export const getCurrentScreenName = (store) => {
-  const drawerRoute = store[NAVIGATION].routes[0];
-  const drawerInnerRoutes = drawerRoute.routes[0];
-  const currentRouteIndex = drawerInnerRoutes.index;
-  const currentRoute = drawerInnerRoutes.routes[currentRouteIndex];
-  const currentRouteName = currentRoute.key;
-  return currentRouteName;
+  const innerRoutes = store[NAVIGATION].routes[0];
+  if (innerRoutes && innerRoutes.routes) {
+    const drawerInnerRoutes = innerRoutes.routes[0];
+    const currentRouteIndex = drawerInnerRoutes.index;
+    const currentRoute = drawerInnerRoutes.routes[currentRouteIndex];
+    const currentRouteName = currentRoute.key;
+    return currentRouteName;
+  }
+  return innerRoutes.routeName;
 };
 
 export const GO_BACK = 'Navigation/BACK';
